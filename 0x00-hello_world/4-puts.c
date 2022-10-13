3dfaac82d1b7516b5ecd736bcc503c362a91e285
OzimiWhoke <ozimi11zz@gmail.com>
2022-10-13 15:57:43 -0700

    Printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 
 #include <stdio.h>
 
+/**
+ * main - printing
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
