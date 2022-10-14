5cc250dd459749020d82586554b0c030607b74e0
OzimiWhoke <ozimi11zz@gmail.com>
2022-10-13 17:57:48 -0700

    Data types

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  *
  * main - printing
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write("and that piece of art is useful" "\"- Dora Korpar, 2015-10-19\n");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,22 @@
 
 #include <stdio.h>
 /**
- * main - Data types
+ * main - Entry point for the program
  *
- * Return: Always 0 (Success)
+ * Return: zero if no error, Non-zero value if errors.
  */
 int main(void)
 {
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
+	int;
+	float;
+	double;
+	char;
 
-	printf("Size of int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
-	printf("Size of double: %zu byte(s)\n", sizeof(doubleType));
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of double: %zu byte(s)\n", sizeof(double));
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of long int: %zu byte(s)\n", sizeof(long int);
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int);
 	return (0);
 }
