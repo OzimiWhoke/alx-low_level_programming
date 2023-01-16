1cdd75084b0d3d2e323a6ab08fccc5536967ec8d
OzimiWhoke <ozimi11zz@gmail.com>
2023-01-16 06:16:24 -0800

    Updates on doubly linked lists

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include <studio>
+#include "lists.h"
+
+/**
+ * dlistint_len - count the number of nodes in the linked list
+ * @h: pointer to the beginning of a linked list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; 1++)
+		h = h->next;
+	return (i);
+}
