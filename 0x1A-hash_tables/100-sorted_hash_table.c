3bda12228ca3a50e32c6994f21c8056b54e8a502
OzimiWhoke <ozimi11zz@gmail.com>
2023-02-03 15:14:50 -0800

    Hash table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,80 +1,43 @@
-nclude "hash_tables.h"
-
-
+#include "hash_tables.h"
 
 /**
+ * shash_table_create - creates a sorted hash table
+ * @size: size of the hash table
  *
- *  * shash_table_create - creates a sorted hash table
- *
- *   * @size: size of the hash table
- *
- *    *
- *
- *     * Return: pointer to the new table, or NULL on failure
- *
- *      */
+ * Return: pointer to the new table, or NULL on failure
+ */
 
 shash_table_t *shash_table_create(unsigned long int size)
-
 {
-
-		shash_table_t *sht;
-
-			unsigned long int i;
-
-
-
-				sht = malloc(sizeof(shash_table_t));
-
-					if (sht == NULL)
-
-								return (NULL);
-
-						sht->size = size;
-
-							sht->shead = NULL;
-
-								sht->stail = NULL;
-
-									sht->array = malloc(sizeof(shash_node_t) * size);
-
-										if (sht->array == NULL)
-
-												{
-
-															free(sht);
-
-																	return (NULL);
-
-																		}
-
-											for (i = 0; i < size; i++)
-
-													{
-
-																sht->array[i] = NULL;
-
-																	}
-
-												return (sht);
-
+	shash_table_t *sht;
+	unsigned long int i;
+	
+	sht = malloc(sizeof(shash_table_t));
+	if (sht == NULL)
+		return (NULL);
+	sht->size = size;
+	sht->shead = NULL;
+	sht->stail = NULL;
+	sht->array = malloc(sizeof(shash_node_t) * size);
+	if (sht->array == NULL)
+	{
+		free(sht);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		sht->array[i] = NULL;
+	}
+	return (sht);
 }
 
-
-
 /**
+ * make_shash_node - makes a node for the sorted hash table
+ * @key: key for the data
+ * @value: data to be stored
  *
- *  * make_shash_node - makes a node for the sorted hash table
- *
- *   * @key: key for the data
- *
- *    * @value: data to be stored
- *
- *     *
- *
- *      * Return: pointer to the new node, or NULL on failure
- *
- *       */
+ * Return: pointer to the new node, or NULL on failure
+ */
 
 shash_node_t *make_shash_node(const char *key, const char *value)
 
